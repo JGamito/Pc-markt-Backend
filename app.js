a1f08962ef636f57b7554cf8a98a00e366ac38de
alfonsofdelarosa <alfonsofdelarosa@gmail.com>
2022-03-16 19:29:41 +0100

    lista de productos

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const app = express()
 const port = process.env.PORT || 8000;
+const Product = require('./src/routes/products');
 
 app.use(express.json());
 
-app.get('/',(req, res, next)=>{
-    res.json({message:'Its working'})
-})
-
+app.use(' /products', Product);
 
 app.listen(port, '', ()=>{
 console.log(`App running on port: ${port}`)
--- src/services/ProductsService.js
+++ src/services/ProductsService.js
@@ -1,9 +1,11 @@
+
 const {Product} = requiere('./../models/mongoose');
+const checker = require('../services/errors');
 
 
 const readAll = async () => {
     return await Product.find();
-  }
+  };
 
 
   module.exports = {
