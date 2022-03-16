9acae70e8eaf636cd1ec5918ac6179df4322598f
JGamito <gamito90@hotmail.com>
2022-03-16 20:15:13 +0100

    correct mongoose file

--- src/models/mongoose.js
+++ src/models/mongoose.js
@@ -1,18 +1,21 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+const mongoose = require("mongoose");
+require("dotenv").config();
 const DB_URL = process.env.DB_URL;
-const DATABASE= process.env.DATABASE;
+const DATABASE = process.env.DATABASE;
 
-mongoose.connect(`${DB_URL}/${DATABASE}`)
+mongoose.connect(`${DB_URL}/${DATABASE}`);
 
-const categorySchema =require('./schemas/categories.js');
-const Category = mongoose.model('categories', categorySchema);
+const categorySchema = require("./schemas/categories.js");
+const Category = mongoose.model("categories", categorySchema);
 
 const brandsSchema = require("./schemas/brands");
 const Brand = mongoose.model("Brand", brandsSchema);
 
+const productsSchemas = require("./schemas/products");
+const Product = mongoose.model("Products", productsSchemas);
+
 module.exports = {
   Brand,
-   Category,
+  Category,
+  Product,
 };
-
