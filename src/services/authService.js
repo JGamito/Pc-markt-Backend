410fac3ef6b76fce320b0a35bbcb3377aa32c771
Ion Sora <soraioniosif@gmail.com>
2022-03-23 21:16:06 +0100

    fix

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,7 +1,7 @@
-const UserService = require('./UserService');
+const UserService = require("./UserService");
 
-const { comparePasswords, encryptPassword } = require('./../helpers/password');
-const checker = require('./errors');
+const { comparePasswords, encryptPassword } = require("./../helpers/password");
+const checker = require("./errors");
 
 const register = async ({ email, password }) => {
   let user = await UserService.findByEmail(email);
@@ -13,27 +13,25 @@ const register = async ({ email, password }) => {
     }
   }
   const hashedPassword = await encryptPassword(password);
-  console.log(hashedPassword)
-  user = await UserService.create({ email, password:hashedPassword});
+  user = await UserService.create({ email, password: hashedPassword });
   return user;
-}
+};
 
 const login = async ({ email, password }) => {
   const user = await UserService.findByEmail(email);
   if (!user) {
     throw await checker.throwErrorIfUserDoesNotExist({ email });
   }
-  console.log(user)
+
   const equalPasswords = await comparePasswords({
     hash: user.password,
     plain: password,
   });
-  console.log(user)
   if (equalPasswords) return user;
   throw await checker.throwIfPasswordsDontMatch();
-}
+};
 
 module.exports = {
   register,
   login,
-}
+};
