6acdf3be4e4c27532e862de806e5f795cf63c02a
alfonsofdelarosa <alfonsofdelarosa@gmail.com>
2022-04-06 20:02:20 +0200

    change productService

--- src/services/ProductsService.js
+++ src/services/ProductsService.js
@@ -1,13 +1,12 @@
 const { Product } = require("./../models/mongoose");
 const checker = require("../services/errors");
-const products = require("../models/schemas/products");
 
 const readAll = async () => {
   return await Product.find();
 };
 
 const read = async (id) => {
-  await checker.throwErrorIfDocumentDoesNotExist({ id, model: Todo });
+  await checker.throwErrorIfDocumentDoesNotExist({ id, model:Product  });
   return await Product.findById(id);
 };
 
